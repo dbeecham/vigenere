5d9061cc390636b051ba3d1007ee44aa31169c33
Daniel Beecham <daniel@lunix.se>
2014-12-28 14:40:29 +0100

    Forgot a file.

--- vigenere.hs
+++ vigenere.hs
@@ -1,7 +1,30 @@
-import Data.Char
+-- A simple Vigenère cipher. Takes a password as argument and encrypts
+-- stdin using the password.
 
-vigenere cipher str = map (chr . (add 97) . ((flip mod) 26)) $ shift (asABList cipher) (asABList str) where
-    asABList = map ((sub 97) . ord)
-    sub x y = y - x
-    add x y = y + x
-shift xs ys = zipWith (+) (cycle xs) ys
+
+import Data.Char
+import System.Environment
+import System.IO
+
+shift _ [] = []
+shift (c:cs) (x:xs)
+    | x > 0 && x < 24 = (c + x) `mod` 24 : shift cs xs
+    | otherwise = x : shift (c:cs) xs
+
+vigenere cipher str = charlist $ shift (cycle (numlist cipher)) (numlist str) where
+    sub a b = b - a
+    numlist = map ((sub 97) . ord . toLower)
+    charlist = map (chr . (+ 97))
+
+printHelp = do
+    name <- getProgName
+    putStrLn $ "Usage: " ++ name ++ " <keyword>"
+    putStrLn ""
+    putStrLn "Encrypts stdin using a vigenere cipher with <keyword> as"
+    putStrLn "passcode."
+
+main = do
+    args <- getArgs
+    let cipher = foldr (++) "" args
+    str <- getContents
+    putStrLn (vigenere cipher str)
